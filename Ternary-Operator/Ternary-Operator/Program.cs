e9214bb14bc2236d0c83693c279a5090ed7c99d3
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-09-02 23:30:58 +0600

    Add files via upload

---
+++ Ternary-Operator/Ternary-Operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ternary_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20;
+            string b = (a > 10) ? "A is greater " : "A is lesser";
+            Console.WriteLine(b);
+        }
+    }
+}
