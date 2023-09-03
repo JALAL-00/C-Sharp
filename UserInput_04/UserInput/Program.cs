436e298b0259480966f1284a83a6c53db2d7a6d3
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-09-04 03:05:49 +0600

    Add files via upload

---
+++ UserInput_04/UserInput/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInput
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Your name is: " + name);
+            Console.WriteLine();
+
+            Console.WriteLine("How old are you? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your are " + age + " Old");
+        }
+    }
+}
