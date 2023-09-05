64a03ec3c5197fdf11cc8bc944efecf1d4cf6ca5
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-09-06 03:53:54 +0600

    Add files via upload

---
+++ Operators_05/Arithmetic-Operator/ArithmeticOperation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmeticOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int c, d, e, f;
+            c = a + b;
+            d = b - a;
+            e = a * b;
+            f = a / b;
+            Console.WriteLine("Addition is: " + c);
+            Console.WriteLine("Subtration is: " + d);
+            Console.WriteLine("Multiplication is: " + e);
+            Console.WriteLine("Devision is: " + f);
+
+
+            //int Friends = 10;
+
+            //Friends = Friends + 1;
+            //Friends += 1;
+            //Friends++;
+
+            //Friends = Friends - 1;
+            //Friends -= 1;
+            //Friends--;
+
+            //Friends = Friends * 2;
+            //Friends *= 2;
+            //Friends * *;
+
+            //Friends = Friends / 2;
+            //Friends /=2;
+
+            //int reminder = Friends % 3;
+
+            //Console.WriteLine(Friends);
+            //Console.WriteLine(reminder);
+        }
+    }
+}
