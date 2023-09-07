9790636d58835137a645c248bf206477c3d2c6d7
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-09-07 23:43:59 +0600

    Add files via upload

---
+++ Conditional_Statement_06/Conditional Statement/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if(age > 100)
+            {
+                Console.WriteLine("You are too old");
+                
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You can vote");
+            }
+            else
+            {
+                Console.WriteLine("You can't vote bcz you are teen");
+            }
+        }
+    }
+}
