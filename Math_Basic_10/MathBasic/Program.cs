7a7773204df95063da579037e02f04c080353766
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-09-22 01:18:02 +0600

    Add files via upload

---
+++ Math_Basic_10/MathBasic/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathBasic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3.444;
+            double y = 6;
+            double z = -5;
+            
+
+            double a = Math.Pow(x, 2);
+            double b = Math.Sqrt(x);
+            double c = Math.Abs(z);
+            double d = Math.Round(x);
+            double e = Math.Ceiling(x);
+            double f = Math.Floor(x);
+            double g = Math.Max(x, y);
+            double h = Math.Min(x, y);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+        }
+    }
+}
