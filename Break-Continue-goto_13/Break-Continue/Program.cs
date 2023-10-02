6275c14bd5ccda9653cdf30fd76d43f581a6d3c0
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-10-03 01:54:15 +0600

    Add files via upload

---
+++ Break-Continue-goto_13/Break-Continue/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Break_Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Break statements
+
+            //for(int i =0; i< 10; i++)
+            //{
+            //    if(i == 5)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Loop Terminate");
+
+            //--------------------------------------//
+
+            //Continue statements
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i == 5)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Loop Terminate");
+
+            //-----------------------------------------//
+
+            //Odd no check
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Loop Terminate");
+
+            //-------------------------------------------//
+
+            //even no check
+            //for (int i = 1; i < 10; i++)
+            //{
+            //    if (i % 2 == 1)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine(i);
+            //}
+            //Console.WriteLine("Loop Terminate");
+
+            //------------------------------------------//
+            //goto statements
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    goto stop;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Loop Terminate");
+
+            stop:
+            Console.WriteLine("Program exit");
+
+        }
+    }
+}
