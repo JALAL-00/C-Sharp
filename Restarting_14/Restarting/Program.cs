625ea52b3f1c4b3bdd288a03eef41a9f04e07288
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-10-04 02:42:10 +0600

    Add files via upload

---
+++ Restarting_14/Restarting/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restarting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string confirm;
+            //do
+            //{
+            //    Console.Write("Enter 1st no: ");
+            //    int no1 = int.Parse(Console.ReadLine());
+            //    Console.Write("Enter 2nd no: ");
+            //    int no2 = int.Parse(Console.ReadLine());
+            //    int add = no1 + no2;
+            //    Console.WriteLine("Addition is: " + add);
+            //    Console.Write("Do you want to reapeat ur program? YES/NO: ");
+            //    confirm = Console.ReadLine().ToLower();
+            //}
+            //while (confirm == "yes");
+
+            while (true)
+            {
+                Console.Write("Enter 1st no: ");
+                int no1 = int.Parse(Console.ReadLine());
+                Console.Write("Enter 2nd no: ");
+                int no2 = int.Parse(Console.ReadLine());
+                int add = no1 + no2;
+                Console.WriteLine("Addition is: " + add);
+                Console.Write("Do you want to reapeat ur program? YES/NO: ");
+                string confirm = Console.ReadLine().ToLower();
+
+                if(confirm == "yes")
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+        }
+    }
+}
