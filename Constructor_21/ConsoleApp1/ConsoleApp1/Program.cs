ae9266718418b41e466069e9d90f500a334ea1e0
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-11-07 01:40:20 +0600

    Add files via upload

---
+++ Constructor_21/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        //public Program()
+        //{
+        //    Console.WriteLine("default constructor invoks !!");
+        //}
+        //static void main(string[] args)
+        //{
+        //    Program p = new Program();
+
+        //}
+
+        //Parameterize Constructor
+
+        int id;
+        string name;
+        int age;
+        public Program(int id, string name, int age)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public string getName()
+        {
+            return this.name;
+        }
+        public int getAge()
+        {
+            return this.age;
+        }
+
+        static void Main(string[] args)
+        {
+            Program Jalal = new Program(12, "Jalal", 23);
+            Console.WriteLine(Jalal.getId());
+            Console.WriteLine(Jalal.getName());
+            Console.WriteLine(Jalal.getAge());
+        }
+    }
+}
