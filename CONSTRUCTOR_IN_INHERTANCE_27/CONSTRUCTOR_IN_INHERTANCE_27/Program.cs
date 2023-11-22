c3b69cbc4399fcf9cfcd26cec8bb2cf2892216c2
JALAL UDDIN <103830450+JALAL-00@users.noreply.github.com>
2023-11-22 08:10:14 +0600

    Add files via upload

---
+++ CONSTRUCTOR_IN_INHERTANCE_27/CONSTRUCTOR_IN_INHERTANCE_27/Program.cs
@@ -0,0 +1,67 @@
+﻿/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTOR_IN_INHERTANCE_27
+{
+    internal class Program
+    {
+        public class Parents
+        {
+            public Parents()
+            {
+                Console.WriteLine("Base Constructor is called");
+            }
+        }
+
+        public class Childs : Parents
+        {
+            public Childs()
+            {
+                Console.WriteLine("Derive Constructor is called");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Childs C = new Childs();
+
+        }
+    }
+}
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONSTRUCTOR_IN_INHERTANCE_27
+{
+    internal class Program
+    {
+        public class Parents
+        {
+            public Parents(string massage)
+            {
+                Console.WriteLine(massage);
+            }
+        }
+
+        public class Childs : Parents
+        {
+            public Childs() : base("Base Constructor")
+            {
+                Console.WriteLine("Derive Constructor is called");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Childs C = new Childs();
+
+        }
+    }
+}
